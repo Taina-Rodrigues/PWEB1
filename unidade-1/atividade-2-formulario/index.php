d930310409197e5e79c17abdae0da63981f32cff
Tainá Rodrigues <taina.rodrigues07@aluno.ifce.edu.br>
2026-03-31 18:18:48 +0000

    Formulário com Validação

--- unidade-1/atividade-2-formulario/index.php
+++ unidade-1/atividade-2-formulario/index.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> Cadastro de Aluno </title>
+</head>
+<body>
 
+<h2> Cadastro para Evento Acadêmico </h2>
+
+<form method="post">
+    Nome: <input type="text" name="nome"><br><br>
+    E-mail: <input type="email" name="email"><br><br>
+    Curso: <input type="text" name="curso"><br><br>
+    Turno: <input type="text" name="turno"><br><br>
+
+    <button type="submit">Cadastrar</button>
+</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $curso = $_POST["curso"];
+    $turno = $_POST["turno"];
+
+    if (empty($nome) || empty($email) || empty($curso) || empty($turno)) {
+        echo "<p style='color:red;'>Erro: Preencha todos os campos!</p>";
+    } else {
+        echo "<h3>Cadastro realizado com sucesso!</h3>";
+        echo "Nome: $nome <br>";
+        echo "E-mail: $email <br>";
+        echo "Curso: $curso <br>";
+        echo "Turno: $turno <br>";
+    }
+}
+?>
+
+</body>
+</html>
