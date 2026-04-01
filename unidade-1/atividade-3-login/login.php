db8b39be85a92868e32862fae9283ff8a5f677b8
Tainá Rodrigues <taina.rodrigues07@aluno.ifce.edu.br>
2026-04-01 01:25:08 +0000

    Sistema de Login com Área Restrita

--- unidade-1/atividade-3-login/login.php
+++ unidade-1/atividade-3-login/login.php
@@ -1,1 +1,24 @@
+<?php
+session_start();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+
+    if ($usuario == "admin" && $senha == "1234") {
+        $_SESSION["logado"] = true;
+        header("Location: painel.php");
+        exit();
+    } else {
+        echo "<p style='color:red;'>Login inválido!</p>";
+    }
+}
+?>
+
+<h2>Login</h2>
+<form method="post">
+    Usuário: <input type="text" name="usuario"><br><br>
+    Senha: <input type="password" name="senha"><br><br>
+    <button type="submit">Entrar</button>
+</form>
