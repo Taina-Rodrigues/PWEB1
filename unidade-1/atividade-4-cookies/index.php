96bcd1595f30d363c7ef68c297ef39d057e96377
Tainá Rodrigues <taina.rodrigues07@aluno.ifce.edu.br>
2026-04-01 01:36:54 +0000

    Controle de Acesso com Cookies

--- unidade-1/atividade-4-cookies/index.php
+++ unidade-1/atividade-4-cookies/index.php
@@ -1,1 +1,24 @@
+<?php
+if (isset($_COOKIE['nome'])) {
+    echo "<h2>Bem-Vindo novamente, " . $_COOKIE['nome'] . "!</h2>";
 
+} else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $nome = $_POST['nome'];
+
+        setcookie("nome", $nome, time() + (7 * 24 * 60 * 60));
+        echo "<p>Nome salvo! Recarregue a página.</p>";
+
+    } else {
+
+        echo '
+        <h2>Digite seu nome:</h2>
+        <form method="post">
+            <input type="text" name="nome" required>
+            <button type="submit">Salvar</button>
+        </form>
+        ';
+    }
+}
+?>
