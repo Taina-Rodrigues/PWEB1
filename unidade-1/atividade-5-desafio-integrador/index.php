c3c22b021c16d2b8c3a59557b8edd0c1bc11ad63
Tainá Rodrigues <taina.rodrigues07@aluno.ifce.edu.br>
2026-04-01 01:43:41 +0000

    Sistema integrador com Login, Cadastro e Sessão

--- unidade-1/atividade-5-desafio-integrador/index.php
+++ unidade-1/atividade-5-desafio-integrador/index.php
@@ -1,1 +1,76 @@
+<?php
+session_start();
 
+function validar($nome) {
+    return !empty($nome);
+}
+
+// Fazer Login
+if (isset($_POST["login"])) {
+    if ($_POST["usuario"] == "admin" && $_POST["senha"] == "1234") {
+        $_SESSION["logado"] = true;
+    } else {
+        echo "<p style='color:red;'>Login inválido!</p>";
+    }
+}
+
+// Fazer Logout
+if (isset($_GET["logout"])) {
+    session_destroy();
+    header("Location: index.php");
+    exit();
+}
+
+// Cadastro
+if (isset($_POST["cadastrar"])) {
+
+    if (!isset($_SESSION["dados"])) {
+        $_SESSION["dados"] = [];
+    }
+
+    $nome = $_POST["nome"];
+
+    if (validar($nome)) {
+        $_SESSION["dados"][] = $nome;
+    } else {
+        echo "<p style='color:red;'>Digite um nome válido!</p>";
+    }
+}
+?>
+
+<h2>Sistema Integrado</h2>
+
+<?php if (!isset($_SESSION["logado"])) { ?>
+
+    <h3>Login</h3>
+    <form method="post">
+        Usuário: <input name="usuario"><br><br>
+        Senha: <input type="password" name="senha"><br><br>
+        <button name="login">Entrar</button>
+    </form>
+
+<?php } else { ?>
+
+    <h3>Bem-vindo!</h3>
+    <a href="?logout=true">Sair</a>
+
+    <h3>Cadastro de nomes</h3>
+    <form method="post">
+        Nome: <input name="nome">
+        <button name="cadastrar">Cadastrar</button>
+    </form>
+
+    <h3>Registros:</h3>
+
+    <?php
+    if (!empty($_SESSION["dados"])) {
+        foreach ($_SESSION["dados"] as $dado) {
+            echo $dado . "<br>";
+        }
+    } else {
+        echo "Nenhum registro ainda.";
+    }
+    ?>
+<?php 
+} 
+?>
