ea96a5c04df57ae4a3775313c8b061d821ec0092
Tainá Rodrigues <taina.rodrigues07@aluno.ifce.edu.br>
2026-06-17 02:47:51 +0000

    fix: simplifica migration foreignId removendo logica de copia de dados

--- lab_migrations/database/migrations/2026_06_17_022220_add_categoria_id_foreignid_to_produtos_table.php
+++ lab_migrations/database/migrations/2026_06_17_022220_add_categoria_id_foreignid_to_produtos_table.php
@@ -1,40 +1,20 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-
 class AddCategoriaIdForeignidToProdutosTable extends Migration
 {
     public function up()
     {
-        Schema::create('produtos_new', function (Blueprint $table) {
+        Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->foreignId('categoria_id')->constrained('categorias');
             $table->timestamps();
         });
-
-        DB::table('produtos_new')->insertUsing(['nome', 'categoria_id'], DB::table('produtos')->select('nome', 'categoria_id'));
-
-        Schema::dropIfExists('produtos');
-        Schema::rename('produtos_new', 'produtos');
     }
-
     public function down()
     {
-        Schema::create('produtos_old', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome');
-            $table->unsignedBigInteger('categoria_id');
-            $table->foreign('categoria_id')->references('id')->on('categorias');
-            $table->timestamps();
-        });
-
-        DB::table('produtos_old')->insertUsing(['nome', 'categoria_id'], DB::table('produtos')->select('nome', 'categoria_id'));
-
         Schema::dropIfExists('produtos');
-        Schema::rename('produtos_old', 'produtos');
     }
 }
